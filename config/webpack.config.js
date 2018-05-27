e1179985bbf6b331953975dd13f6d97b943076da
lcluber <ludovic.cluber@gmail.com>
2018-05-27 12:28:25 +0200

    weird eval()

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,9 +2,13 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
+  target: 'web', // enum
   entry: './website/js/example.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, '../website/public/js/')
-  }
+  }//,
+  // externals: {
+  //   jquery: 'jQuery'
+  // }
 };
